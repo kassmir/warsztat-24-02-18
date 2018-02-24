59cdc79c0744415300c0219e1699e20aae54ac30
Katarzyna Rozmysł <kassmir@gmail.com>
2018-02-24 12:28:33 +0100

    Data types - primitives

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('Hello Kasia!')
+
+var textVariable = "Kasia"
+console.log(textVariable)
+
+var numberVariable = 88
+console.log(numberVariable)
+
+//inny typ zmiennej niż string i number - boolean (true/false)
+var boolVariable = true
+console.log(boolVariable)
+
+//null
+var nullVariable = null
+console.log(nullVariable)
+
+//undefined
+var undefinedVariable = undefined
+console.log(undefinedVariable)

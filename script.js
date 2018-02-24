4957df1fefddbda429b1afb632e76beffae5af79
Katarzyna Rozmysł <kassmir@gmail.com>
2018-02-24 13:48:34 +0100

    Cykliczne wywoływanie

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
-function hello(name, numberOfLogs) { //parameter is a name for attribute 
-    for (var i = 0; i < numberOfLogs; i++) {
-        console.log('Hello ' + name + '!')
+//wykonywanie kodu cyklicznie
+
+setInterval(
+    function() {
+        console.log('Minęła sekunda!')
+    },
+    1000
+)
+
+//zakres widoczności zmiennych (globalne i lokalne)
+
+function main() {
+    var secondsCounter = 1
+
+    var intervalId = setInterval(
+        function() {
+            console.log(secondsCounter++)
+        },
+        1000
+    )
+
+    setTimeout {
+        function() {
+            clearInterval(intervalId)
+        },
+        5000
     }
 }
-hello('Kasia', 1)
-hello('Leszek', 2)
-hello('Darek', 3)
+
+main()
+
+//aby coś było potraktowane jako wyrażenie, a nie deklaracja, należy to napisać w nawiasach ()
+
+(function() {
+    var secondsCounter = 1
+
+    var intervalId = setInterval(
+        function() {
+            console.log(secondsCounter++)
+        },
+        1000
+    )
+
+    setTimeout {
+        function() {
+            clearInterval(intervalId)
+        },
+        5000
+    }
+})

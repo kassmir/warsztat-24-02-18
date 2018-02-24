5077860d3176e3a566f054c08a1cdb2af1d492bb
Katarzyna Rozmysł <kassmir@gmail.com>
2018-02-24 15:02:50 +0100

    oneclick Event Listener

--- script.js
+++ script.js
@@ -1,50 +1,10 @@
-//wykonywanie kodu cyklicznie
+var body = document.querySelector('body')
 
-setInterval(
+console.log(body)
+
+body.addEventListener(
+    'click',
     function() {
-        console.log('Minęła sekunda!')
-    },
-    1000
+        console.log("Klik!")
+    }
 )
-
-//zakres widoczności zmiennych (globalne i lokalne)
-
-function main() {
-    var secondsCounter = 1
-
-    var intervalId = setInterval(
-        function() {
-            console.log(secondsCounter++)
-        },
-        1000
-    )
-
-    setTimeout {
-        function() {
-            clearInterval(intervalId)
-        },
-        5000
-    }
-}
-
-main()
-
-//aby coś było potraktowane jako wyrażenie, a nie deklaracja, należy to napisać w nawiasach ()
-
-(function() {
-    var secondsCounter = 1
-
-    var intervalId = setInterval(
-        function() {
-            console.log(secondsCounter++)
-        },
-        1000
-    )
-
-    setTimeout {
-        function() {
-            clearInterval(intervalId)
-        },
-        5000
-    }
-})

ff67875c66ee0db2e964f8008af6a9d1d9048a4d
Katarzyna Rozmysł <kassmir@gmail.com>
2018-02-24 13:09:17 +0100

    Functions

--- script.js
+++ script.js
@@ -1,58 +1,28 @@
-console.log('Hello Kasia!')
+function hello() {
+    console.log('Hello!')
+    console.log('Hello!')
+    console.log('Hello!')
 
-var textVariable = "Kasia"
-console.log(textVariable)
-
-var numberVariable = 88
-console.log(numberVariable)
-
-//inny typ zmiennej niż string i number - boolean (true/false)
-var boolVariable = true
-console.log(boolVariable)
-
-//null
-var nullVariable = null
-console.log(nullVariable)
-
-//undefined
-var undefinedVariable = undefined
-console.log(undefinedVariable)
-
-
-
-//objects
-
-//object with data
-var simplestObj = {
-    name: "Kasia",
-    lastname: "Rozmysł"
 }
-console.log(simplestObj)
+hello()
 
-//nested object with data
-var nestedObj = {
-    name: "Kasia",
-    car: {
-        brand: "Citroen",
-        model: "C3"
-    }
+
+function hello(name) { //parameter is a name for attribute 
+    console.log('Hello ' + name + '!')
+    console.log('Hello ' + name + '!')
+    console.log('Hello ' + name + '!')
+
 }
-console.log(nestedObj)
-console.log(nestedObj.car.model) //do każdej kolejnej właściwości odwołujemy się po kropce
-console.log(nestedObj.car.year)
+hello('Kasia')
+hello('Leszek')
+hello('Darek')
 
 
+/*Funkcje warte zapoznania:
+forEach 
+map
+filter
+reduce
 
-//arrays (tablice)
-var simpleArray = [
-    1,
-    { name: 'Basia' },
-    2,
-    { array: [1, 3, 7] },
-    3,
-    4,
-    5
-]
-console.log(simpleArray)
-console.log(simpleArray[4])
-console.log(simpleArray[1].name) //inną możliwością zamiast odwołania po kropce jest nawias kwadratowy ['name']
+kanał na youtube do funkcji i JS: fun fun function (blok: functional programming)
+*/

55a28d0d457edf1fc99b9c751821dcfbf420c21e
Katarzyna Rozmysł <kassmir@gmail.com>
2018-02-24 12:50:36 +0100

    Objects

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ console.log(nullVariable)
 //undefined
 var undefinedVariable = undefined
 console.log(undefinedVariable)
+
+
+
+//objects
+
+//object with data
+var simplestObj = {
+    name: "Kasia",
+    lastname: "Rozmysł"
+}
+console.log(simplestObj)
+
+//nested object with data
+var nestedObj = {
+    name: "Kasia",
+    car: {
+        brand: "Citroen",
+        model: "C3"
+    }
+}
+console.log(nestedObj)
+console.log(nestedObj.car.model) //do każdej kolejnej właściwości odwołujemy się po kropce
+console.log(nestedObj.car.year)
+
+
+
+//arrays (tablice)
+var simpleArray = [
+    1,
+    { name: 'Basia' },
+    2,
+    { array: [1, 3, 7] },
+    3,
+    4,
+    5
+]
+console.log(simpleArray)
+console.log(simpleArray[4])
+console.log(simpleArray[1].name) //inną możliwością zamiast odwołania po kropce jest nawias kwadratowy ['name']
